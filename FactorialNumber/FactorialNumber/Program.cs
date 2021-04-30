8a16f55a40182b1f7d5628f4f8b10e0da003df59
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-04-30 10:57:54 +0530

    Add files via upload

---
+++ FactorialNumber/FactorialNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FactorialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FINDING THE FACTORIAL OF NUMBER");
+            Console.WriteLine("Enter the Number:");
+            int n = int.Parse(Console.ReadLine());
+            int f = 1,temp=n;
+            for(int i = 1; i < n; i++)
+            {
+                f = f * temp;
+                temp--;
+            }
+            Console.WriteLine("The Factorial of " + n + " is " + f);
+            Console.ReadLine();
+        }
+    }
+}
