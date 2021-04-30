a366bdd89b412f84181186a5a8bc67b1ad9158d9
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-04-30 10:44:49 +0530

    Add files via upload

---
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number " + n + " is EVEN ");
+            }
+            else
+            {
+                Console.WriteLine("The number " + n + " is ODD ");
+            }
+        }
+    }
+}
