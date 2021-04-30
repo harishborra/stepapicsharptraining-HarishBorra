b59a35f1b93753478bdaf04907f0515257dd2827
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-04-30 10:49:24 +0530

    Add files via upload

---
+++ FibnocciSeries/FibnocciSeries/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FibnocciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number upto which we need the series :");
+            int n = int.Parse(Console.ReadLine());
+            int i, f1 = 0, f2 = 1,f3;
+            Console.Write(f1+" ");
+            Console.Write(f2+" ");
+            for (i = 2; i < n; i++)
+            {
+                f3 = f1 + f2;
+                Console.Write(f3+" ");
+                f1 = f2;
+                f2 = f3;
+            }
+        }
+    }
+}
