0afd3a7db647fc301c2d4711587e332bc643005a
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-04-30 10:45:53 +0530

    Add files via upload

---
+++ FirstProject/FirstProject/ExplicitConDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace FirstProject
+{
+    class ExplicitConDemo
+    {
+        public ExplicitConDemo()
+        {
+            Console.WriteLine("Constructor is Called");
+        }
+        public static void Main()
+        {
+            ExplicitConDemo obj = new ExplicitConDemo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        int i;bool b;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("The Value of i is: "+p.i);
+            Console.WriteLine("The Value of b is: "+p.b);
+
+            Console.ReadLine();
+        }
+    }
+}
