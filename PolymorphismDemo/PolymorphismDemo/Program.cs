06a129e0f2be4821898631793e7a1bff7de080e3
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-05-18 18:06:41 +0530

    Add files via upload

---
+++ PolymorphismDemo/PolymorphismDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PolymorphismDemo
+{
+    
+    class Program
+    {
+        public class Shape
+        {
+            public virtual void draw()
+            {
+                Console.WriteLine("Started drawing..");
+            }
+        }
+        public class Rectangle : Shape
+        {
+            public override void draw()
+            {
+                Console.WriteLine("Inside the Rectangle and the rectangle is drawn.");
+            }
+
+        }
+        public class Circle : Shape
+        {
+            public override void draw()
+            {
+                Console.WriteLine("Inside the Circle and drawing of circle is done");
+            }
+
+        }
+        public class TestPolymorphismDemo
+        {
+            public static void Main()
+            {
+                Shape s = new Shape();
+                s.draw();
+                s = new Rectangle();
+                s.draw();
+                s = new Circle();
+                s.draw();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
