e80ee435f123b24021d99bc7dd6f2ae31722710f
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-06-09 22:27:48 -0700

    Add files via upload

---
+++ Project/Models/BasicData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CovidData.Models
+{
+    public class BasicData
+    {
+        public int dailyconfirmed { get; set; }
+        public int dailydeceased { get; set; }
+        public int dailyrecovered { get; set; }
+        public string date { get; set; }
+        public DateTime? dateymd { get; set; }
+        public int totalconfirmed { get; set; }
+        public int totaldeceased { get; set; }
+        public int totalrecovered { get; set; }
+    }
+}
