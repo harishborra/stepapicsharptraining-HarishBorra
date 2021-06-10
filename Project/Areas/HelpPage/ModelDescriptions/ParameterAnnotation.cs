341bb4e9c937cb35bf1470f1f2a2b9cce0b08b51
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-06-09 22:22:36 -0700

    Add files via upload

---
+++ Project/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CovidData.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
