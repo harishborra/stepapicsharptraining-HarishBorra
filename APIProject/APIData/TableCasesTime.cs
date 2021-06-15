7aa96eb77776d3ba6b823d52a906155d240d59e6
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-06-15 10:54:23 +0530

    Add files via upload

---
+++ APIProject/APIData/TableCasesTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIProject
+{
+    public class TableCasesTime
+    {
+        public string dailyconfirmed { get; set; }
+        public string dailydeceased { get; set; }
+        public string dailyrecovered { get; set; }
+        public string date { get; set; }
+        public string dateymd { get; set; }
+        public string totalconfirmed { get; set; }
+        public string totaldeceased { get; set; }
+        public string totalrecovered { get; set; }
+    }
+}
