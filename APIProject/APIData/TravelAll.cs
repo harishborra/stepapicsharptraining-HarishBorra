f94dc7e74d09cc65e01d44a823484ab826321745
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-06-15 10:55:01 +0530

    Add files via upload

---
+++ APIProject/APIData/TravelAll.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProject
+{
+    class TravelAll
+    {
+        [JsonProperty("cases_time_series")]
+        public TableCasesTime[] casesall { get; set; }
+    }
+}
