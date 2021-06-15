9c6f0af41caedad10a43081293686f58827749aa
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-06-15 10:47:43 +0530

    Add files via upload

---
+++ APIProject/APICrudOperations/Models/TravelAll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace APICrudOperations.Models
+{
+    public class TravelAll
+    {
+        [JsonProperty("travel_history")]
+        public TableTravel[] all { get; set; }
+
+    }
+}
