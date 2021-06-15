7b1b8984848c306626c922a4a38782252a54753b
Harish Borra <66582446+harishborra@users.noreply.github.com>
2021-06-15 10:47:14 +0530

    Add files via upload

---
+++ APIProject/APICrudOperations/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APICrudOperations.Models;
+
+namespace APICrudOperations.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [RoutePrefix("travel")]
+        public class TravelController : ApiController
+        {
+            DapperConnection mydapper = new DapperConnection();
+
+            [Route("getall")]
+            [HttpGet]
+            public IEnumerable<TableTravel> GetAllEmployees()
+            {
+                return mydapper.GetAll();
+            }
+
+            [Route("{id}")]
+
+            public TableTravel Get(int id)
+            {
+                return mydapper.GetId(id);
+            }
+            [Route("add")]
+            [HttpPost]
+            public void Post([FromBody] TableTravel prod)
+            {
+                mydapper.AddData(prod);
+            }
+
+            [Route("update")]
+            [HttpPut]
+            public void Put([FromBody] TableTravel prod)
+            {
+                mydapper.AddData(prod);
+            }
+
+            [Route("delete/{id}")]
+            [HttpDelete]
+            public void Delete(int id)
+            {
+                mydapper.DeleteData(id);
+            }
+
+        }
+    }
+}
